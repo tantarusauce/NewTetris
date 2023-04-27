3931f3453d52403c96d516e53f626699fa902c28
tantarusauce <104958869+tantarusauce@users.noreply.github.com>
2023-04-27 18:45:54 +0900

    v0.0 タイトルづくり中

---
+++ NewTetris/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Window : Form
+    {
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        public static void Main()
+        {
+            Application.Run(new Window());
+        }
+        Window()
+        {
+            this.Text = "Tetris";
+            this.ClientSize = new Size(1280, 720);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Paint += new PaintEventHandler(form_Paint);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = 15;
+            timer.Start();
+        }
+        public void form_Paint(Object sender, PaintEventArgs e)
+        {
+
+        }
+        public void timer_Tick(Object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
