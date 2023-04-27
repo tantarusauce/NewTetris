b199bdd87c5ae7eea4d14b3faec33017fcb6dbb3
tantarusauce <104958869+tantarusauce@users.noreply.github.com>
2023-04-27 23:43:26 +0900

    v0.0.1

--- NewTetris/Main.cs
+++ NewTetris/Main.cs
@@ -2,11 +2,13 @@
 using System.Windows.Forms;
 using System.Drawing;
 using System.Threading.Tasks;
+using static System.Windows.Forms.DataFormats;
 
 namespace Main
 {
     class Window : Form
     {
+        private bool constr = false;
         System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
         public static void Main()
         {
@@ -20,17 +22,26 @@ namespace Main
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+            TitleClass.Title tit = new TitleClass.Title();
             this.Paint += new PaintEventHandler(form_Paint);
             timer.Tick += new EventHandler(timer_Tick);
             timer.Interval = 15;
             timer.Start();
+            tit.timer_Start();
+
+            tit.Show();
         }
         public void form_Paint(Object sender, PaintEventArgs e)
         {
-
+            
         }
         public void timer_Tick(Object sender, EventArgs e)
         {
+            if (!constr)
+            {
+                this.Visible = false;
+                constr = true;
+            }
             Invalidate();
         }
     }
--- NewTetris/TitleClass.cs
+++ NewTetris/TitleClass.cs
@@ -1,13 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TitleClass
 {
-    internal class Title
+    public class Title : Form
     {
+        private Image img;
+        public bool next;
+        private int i = 0;
+
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        public Title()
+        {
+            this.next = false;
+            this.Text = "Tetris";
+            this.ClientSize = new Size(1280, 720);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.img = Image.FromFile(".\\resources\\background0.png");
+            this.Paint += new PaintEventHandler(form_Paint);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = 15;
+        }
+        public void timer_Start()
+        {
+            timer.Start();
+        }
+        public void timer_Tick(Object sender, EventArgs e)
+        {
+            i++;
+            if(i > 500)
+            {
+                next = true;
+            }
+            Invalidate();
+        }
+        public void form_Paint(Object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(img, 0, 0, 1280, 720);
+        }        
 
     }
 }
